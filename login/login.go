7232094cb0f75eab3684c7f2bc7c85d206dbc88f
Max Matveev <mail@maxmatveev.com>
2015-06-21 23:03:36 +0200

    Initial commit

---
+++ login/login.go
@@ -0,0 +1,24 @@
+package login
+
+import (
+	"fmt"
+	"net/http"
+	"appengine/user"
+	"appengine"
+)
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	fmt.Fprintf(w, "Hi %v!", u)
+}
